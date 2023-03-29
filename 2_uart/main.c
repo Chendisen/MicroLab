effd269f0565a153314cac0a655f16be2a6373ce
Edvard Skei Tønset <edvardskt@gmail.com>
2023-03-29 19:29:41 +0200

    Working uart_send and _receive

--- 2_uart/main.c
+++ 2_uart/main.c
@@ -1,24 +1,35 @@
 #include "gpio.h"
 #include "uart.h"
+#include <stdio.h>
 
 
-int main(void)
+int main()
 {
     gpio_init();
     uart_init();
-    char letter;
     int sleep;
+    int light_status = 0;
 
     while(1){
         if(ubit_button_press_a()){
-            gpio_lights_on();
-            letter = 'A';
-            uart_send(letter);
+            uart_send('A');
         }
         if(ubit_button_press_b()){
-            gpio_lights_off();
-            letter = 'B';
-            uart_send(letter);
+            uart_send('B');
+        }
+
+        if(uart_read() != '\0')
+        {
+            if(light_status == 1)
+            {
+                gpio_lights_off();
+                light_status = 0;
+            }
+            else if(light_status == 0)
+            {
+                gpio_lights_on();
+                light_status = 1;
+            } 
         }
 
         sleep = 10000;
--- 2_uart/uart.c
+++ 2_uart/uart.c
@@ -9,35 +9,33 @@ void uart_init()
     GPIO1->PIN_CNF[8] = 1; // Configure TX as output
 
     // Tell the pin positions to the uart.
-    UART->PSEL_TXD = 6;         
-    UART->PSEL_RXD = 40;  
+    UART->PSEL_TXD = 0x6;         
+    UART->PSEL_RXD = 0x28;  
 
     // Correct Baudrate
     UART->BAUDRATE = 0x00275000;
 
     // Disconnect RTS and CTS
-    UART->PSEL_RTS |= (1 << 31);
-    UART->PSEL_CTS |= (1 << 31);
+    UART->PSEL_RTS = (0x1 << 31);
+    UART->PSEL_CTS = (0x1 << 31);
 
     // Turn on the UART Module
-    UART->ENABLE = 4;
+    UART->ENABLE = 0x4;
 
     // Start UART Receiver
-    UART->TASKS_STARTRX = 1;
+    UART->TASKS_STARTRX = 0x1;
 }
 
 
 void uart_send(char letter)
 {
-    // UART->TASKS_STOPRX = 1;
-    UART->TASKS_STARTTX = 1;
+    UART->TASKS_STARTTX = 0x1;
     
     UART->TXD = letter;
     while(!UART->EVENTS_TXDRDY){
-        continue;
     }
-    UART->EVENTS_TXDRDY = 0;
-    UART->TASKS_STOPTX = 1;
+    UART->EVENTS_TXDRDY = 0x0;
+    UART->TASKS_STOPTX = 0x1;
 }
 
 char uart_read()
